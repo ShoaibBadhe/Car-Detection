24c3b222e8aff5e4a7edcadab73ea225d7886e49
sabihabadhe <114903123+sabihabadhe@users.noreply.github.com>
2022-10-03 12:23:24 +0530

    Update car detection in video.py

--- car detection in video.py
+++ car detection in video.py
@@ -7,7 +7,7 @@ cap = cv2.VideoCapture('cars.mp4')
 while cap.isOpened():
     flag,img = cap.read()
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-    cars = car_cascade.detectMultiScale(gray, 1.05, 10)
+    cars = carCascade.detectMultiScale(gray, 1.05, 10)
     for (x,y,w,h) in cars:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
     cv2.imshow('Car Detector',img)

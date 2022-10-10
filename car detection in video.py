2c1d7fd2b0708582823046cb1ec998a03599d48e
anshul6816 <42142454+anshul6816@users.noreply.github.com>
2022-10-10 21:30:42 +0530

    Update car detection in video.py

--- car detection in video.py
+++ car detection in video.py
@@ -11,4 +11,4 @@ while cap.isOpened():
     for (x,y,w,h) in cars:
         cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
     cv2.imshow('Car Detector',img)
-    cv2.waitKey(1)
+    cv2.waitKey(0)
